dabe676a48f11306777b2764123fa3e7725165c5
Joanne Lee <joanne.lee@berkeley.edu>
2021-02-04 15:23:38 -0800

    commit message

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -22,7 +22,38 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        int[] numbers = {1000,2000,4000,8000,16000,32000,64000,128000};
+        int M = 10000;
+        SLListsOfSizes(numbers,Ns,times,opCounts,M);
+        printTimingTable(Ns, times, opCounts);
+
+    }
+
+    private static void SLListsOfSizes(int[] listSizes, AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts, int ops) {
+        int i = 0;
+        while (i < listSizes.length) {
+            int size = listSizes[i];
+            Ns.addLast(size);
+
+            SLList<Integer> sllist = new SLList<>();
+            while (sllist.size() < size) {
+                sllist.addLast(0);
+            }
+
+            Stopwatch sw = new Stopwatch();
+            for (int op_count = 0; op_count < ops; op_count += 1) {
+                sllist.getLast();
+            }
+            double getlast_time = sw.elapsedTime();
+            times.addLast(getlast_time);
+            opCounts.addLast(ops);
+
+            i += 1;
+        }
     }
 
 }
