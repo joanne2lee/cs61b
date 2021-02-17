799b6e222ad66696cbc5fd0546c509bb494d4cae
Joanne Lee <joanne.lee@berkeley.edu>
2021-02-16 22:28:28 -0800

    proj1 EC

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -1,5 +1,53 @@
 package tester;
 
 import static org.junit.Assert.*;
+
+import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
+
+public class TestArrayDequeEC {
+
+    @Test
+    public void randomizedTest() {
+        StudentArrayDeque<Integer> std = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> sol = new ArrayDequeSolution<>();
+
+        int N = 50000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+
+            if (operationNumber == 0) {
+                // addFirst
+                int randVal = StdRandom.uniform(0, 20);
+                std.addFirst(randVal);
+                sol.addFirst(randVal);
+                assertEquals("addFirst(" + randVal + ")" + "\n" + "get(0)", sol.get(0), std.get(0));
+
+            } else if (operationNumber == 1) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 20);
+                std.addLast(randVal);
+                sol.addLast(randVal);
+                assertEquals("addLast(" + randVal + ")" + "\n" + "get(0)", sol.get(0), std.get(0));
+
+            } else if (operationNumber == 2 && std.size()!= 0 && sol.size()!= 0) {
+                // removeFirst
+                Integer stdF = std.removeFirst();
+                Integer solF = sol.removeFirst();
+                assertEquals("removeFirst()", solF, stdF);
+
+            } else if (operationNumber == 3 && std.size()!= 0 && sol.size()!= 0) {
+                // removeLast
+                Integer stdL = std.removeLast();
+                Integer solL = sol.removeLast();
+                assertEquals("removeLast()", solL, stdL);
+
+            }
+
+        }
+
+
+    }
+}
+
