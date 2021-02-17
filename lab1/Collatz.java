7bd3650e429d2fd6798d145bce308a2b16bcaca9
Joanne Lee <joanne.lee@berkeley.edu>
2021-02-16 21:37:14 -0800

    lab4

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,20 +2,6 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-
-    /**
-     * Buggy implementation of nextNumber!
-     */
-    public static int nextNumber(int n) {
-        if (n == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
-    }
-
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
@@ -24,7 +10,15 @@ public class Collatz {
             n = nextNumber(n);
         }
 
+        }
+    /** Method that returns the next number of the Collatz sequence, given n. */
+    public static int nextNumber(int n) {
+        if (n==1)
+            return n;
+        if (n % 2 == 0)
+            return (n / 2);
+        else
+            return (3 * n + 1);
     }
-    
 }
 
